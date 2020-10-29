45ecffffc793822fcdb709e5da7aa7bf055490e4
Leeroy CAZERES <lcazeres@e3r6p2.42.fr>
2020-10-29 09:22:49 +0100

    struct

--- before_exam/c_try.c
+++ before_exam/c_try.c
@@ -6,7 +6,7 @@
 /*   By: lcazeres <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 09:13:29 by lcazeres          #+#    #+#             */
-/*   Updated: 2020/10/29 09:16:50 by lcazeres         ###   ########.fr       */
+/*   Updated: 2020/10/29 09:22:35 by lcazeres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 typedef struct	s_point
 {
-	int i;
-	int l;
+	int		i;
+	int		l;
+	char	*test;
 }				t_test_i;
+
+int		main()
+{
+	t_test_i tab[4];
+
+	tab[0].i = 5;
+	tab[1].test = "chien";
+	printf("0=%d,  1=%s", tab[0].i, tab[1].test);
+}
